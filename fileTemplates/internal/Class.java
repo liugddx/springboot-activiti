4f10f6db09fa802f069a06e997c7bd01b16c018e
liugd <liugd@si-tech.com.cn>
2019-11-10 16:26:01 +0800

    IU-2018.1.8 <lenovo@liugd Create markdown.xml, project.default.xml, diff.xml, yiiguxing.translation.xml, Default.xml, file.template.settings.xml, ignore.xml, find.xml, keymap.xml, code.style.schemes.xml, _@user_Darcula.icls, test.xml, github_settings.xml, mavenVersion.xml, AiXCoderConfig.xml, Default.xml, activate-power-mode_setting.xml, debugger.renderers.xml, Eclipse copy.xml, MyBatisCodeHelper.xml, editor.codeinsight.xml, OGNL.xml, editor.xml, debugger.xml, baseRefactoring.xml, alibaba-toolkit-tips.xml, ide.general.xml, git.xml, _@user_Default.icls, lombok-plugin.xml, other.xml, GoogleStyle.xml, ui.lnf.xml, filetypes.xml, projectView.xml, Default copy.icls, plugin_ui.xml, colors.scheme.xml, notifications.xml, VisualVMLauncher.xml, databaseDrivers.xml, templates.xml, OGNL _Struts 2_.xml, laf.xml, Class.java, jdk.table.xml, path.macros.xml, mavenRunHelper.xml, proxy.settings.xml, vcs.xml, databaseSettings.xml

---
+++ fileTemplates/internal/Class.java
@@ -0,0 +1,11 @@
+#if (${PACKAGE_NAME} && ${PACKAGE_NAME} != "")package ${PACKAGE_NAME};#end
+#parse("File Header.java")
+/**
+    *<p>@ClassName ${NAME}</p>
+    *<p>@description TODO</p>
+    *@author liugd
+    *@date ${DATE} ${TIME}
+    *@version 1.0
+    */
+public class ${NAME} {
+}
